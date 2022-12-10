a0a458ebcd4811308a082112d81ef034d89eb518
Junseo Oh <insung9546@gmail.com>
2022-12-10 20:30:04 +0900

    Feat: add Github Action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Main() {
-  return <div>메인 입니다.</div>;
+  return <div>메인 입니다. 0.0.1 (업데이트 필요)</div>;
 }
--- src/pages/updater/index.tsx
+++ src/pages/updater/index.tsx
@@ -1,7 +1,9 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { replace } from "../../helpers/window";
 
 export default function Updater() {
+  useUpdate();
+
   return (
     <div>
       <div>로딩중...</div>
@@ -16,4 +18,26 @@ export default function Updater() {
       </div>
     </div>
   );
+}
+
+import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+
+function useUpdate() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const { shouldUpdate, manifest } = await checkUpdate();
+        console.log({ shouldUpdate, manifest });
+        if (shouldUpdate) {
+          // display dialog
+          await installUpdate();
+          // install complete, restart the app
+          await relaunch();
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    })();
+  }, []);
 }
